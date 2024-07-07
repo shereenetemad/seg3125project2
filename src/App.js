e632dad22bf89614d33ac00f2de3cef560f50d3e
Shereen <90640451+shereenetemad@users.noreply.github.com>
2024-07-07 13:17:26 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import ProductOfTheDay from './components/ProductOfTheDay';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/product-of-the-day" component={ProductOfTheDay} />
+          <Route path="/chat" component={Chat} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

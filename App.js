d9635e080ddc45e8ddbe0c30130f0f337f2bfdec
Shereen <90640451+shereenetemad@users.noreply.github.com>
2024-07-07 13:40:05 -0400

    Create App.js
    Signed-off-by: Shereen <90640451+shereenetemad@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,26 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import ProductOfTheDay from './components/ProductOfTheDay';
+import Chat from './components/Chat';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/product-of-the-day" component={ProductOfTheDay} />
+          <Route path="/chat" component={Chat} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

52b8eaa8b429e5e86c631ebe8a84745c645917a8
Shereen <90640451+shereenetemad@users.noreply.github.com>
2024-07-07 21:17:02 -0400

    Update Home.js
    Signed-off-by: Shereen <90640451+shereenetemad@users.noreply.github.com>

--- Home.js
+++ Home.js
@@ -1,24 +1,23 @@
-// src/components/Home.js
 import React from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
   return (
     <div>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/">Glamorize Time</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Glamorize Time</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/makeup">Makeup</Nav.Link>
-              <Nav.Link as={Link} to="/hair-products">Hair Products</Nav.Link>
-              <Nav.Link as={Link} to="/perfumes">Perfumes</Nav.Link>
-              <Nav.Link as={Link} to="/product-of-the-day">Product of the Day</Nav.Link>
-              <Nav.Link as={Link} to="/chat">Chat</Nav.Link>
+              <Nav.Link as={NavLink} to="/" exact activeStyle={{fontWeight: "bold", color: "blue"}}>Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/makeup" activeStyle={{fontWeight: "bold", color: "blue"}}>Makeup</Nav.Link>
+              <Nav.Link as={NavLink} to="/hair-products" activeStyle={{fontWeight: "bold", color: "blue"}}>Hair Products</Nav.Link>
+              <Nav.Link as={NavLink} to="/perfumes" activeStyle={{fontWeight: "bold", color: "blue"}}>Perfumes</Nav.Link>
+              <Nav.Link as={NavLink} to="/product-of-the-day" activeStyle={{fontWeight: "bold", color: "blue"}}>Product of the Day</Nav.Link>
+              <Nav.Link as={NavLink} to="/chat" activeStyle={{fontWeight: "bold", color: "blue"}}>Chat</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>

ef97d748e5107886fd6361f7976b1129ff1801b2
Shereen <90640451+shereenetemad@users.noreply.github.com>
2024-07-21 20:10:13 -0400

    Update BookingService.java
    Signed-off-by: Shereen <90640451+shereenetemad@users.noreply.github.com>

--- BookingService.java
+++ BookingService.java
@@ -5,6 +5,9 @@ import com.example.beautytutorbooking.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
 @Service
 public class BookingService {
 
@@ -13,5 +16,16 @@ public class BookingService {
 
     public void saveBooking(Booking booking) {
         bookingRepository.save(booking);
+    }
+
+    public boolean isValidBooking(Booking booking) {
+        DayOfWeek dayOfWeek = booking.getAppointmentDate().getDayOfWeek();
+        LocalTime appointmentTime = booking.getAppointmentTime();
+        LocalTime closingTime = LocalTime.of(20, 0);
+        LocalTime openingTime = LocalTime.of(8, 0);
+
+        // Check if the appointment is within operating hours and days
+        return !dayOfWeek.equals(DayOfWeek.SATURDAY) && !dayOfWeek.equals(DayOfWeek.SUNDAY)
+                && !appointmentTime.isBefore(openingTime) && !appointmentTime.isAfter(closingTime);
     }
 }
